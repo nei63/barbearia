bdf44413dc79f0f0bcf1f5a61d82b13b60e3ac9f
nei63 <nathanss866@gmail.com>
2022-05-31 20:53:47 -0300

    '3'

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Index from './src/pages/Index/'
-import Information from './src/pages/Information/'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import AppBarber from './src/pages/AppBarber/'
+import ClienteControle from './src/pages/Cliente_Painel_de_Controle/'
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs(){
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name='AppBarber' component={AppBarber}/>
+      <Tab.Screen name='Painel de Controle' component={ClienteControle}/>
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Tela Inicial" component={Index}/>
-        <Stack.Screen name="Information" component={Information}/>
+        <Stack.Screen name="hhhhhh" component={Tabs}/>
+        <Stack.Screen name="ClienteControle" component={ClienteControle}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
